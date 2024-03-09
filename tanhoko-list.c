24faaf7c5952bdcddaeaaf0166a542b11197c1c3
github user name <nakataso1029@gmail.com>
2024-03-09 10:02:41 +0900

    データを表す変数名の変更をする

--- tanhoko-list.c
+++ tanhoko-list.c
@@ -8,13 +8,15 @@ typedef struct {
 
 
 int main() {
-    list hogeta1;
-    list hogeta2;
-    hogeta1.value = 1000;
-    hogeta1.p_next = &hogeta2.value;
-    hogeta2.value = 500;
-    printf("value %d\n", hogeta1.value);
-    printf("position %d\n", hogeta1.p_next);
+    list data1;
+    list data2;
+    
+    
+    data1.value = 1000;
+    data2.p_next = &hogeta2.value;
+    data2.value = 500;
+    printf("value %d\n", data1.value);
+    printf("position %d\n", data1.p_next);
 
 
     return 0;
