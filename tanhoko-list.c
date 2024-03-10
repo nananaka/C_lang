e884e82e8ed9d8a42bb30329a991ed0b76e1bddc
github user name <nakataso1029@gmail.com>
2024-03-10 10:58:38 +0900

    とりあえず、指定した値が検索できるようになった。

--- tanhoko-list.c
+++ tanhoko-list.c
@@ -12,10 +12,9 @@ void show_all_data(list *p_list_head){
     printf("-----------current_list--------------\n");
     while(p_list_pointer != NULL){
         printf("%d",p_list_pointer->value);
-        printf("  (address : %d) \n",p_list_pointer); /*debug*/
+        printf("  (address : %d) \n",p_list_pointer); 
         p_list_pointer = p_list_pointer->p_next;
     }
-    printf("-------------------------------------\n");
 }
 
 
@@ -23,23 +22,42 @@ void show_all_data(list *p_list_head){
 void push_data(list *p_list_head, list *p_new_data){
 
 
-    list *p_list_pointer = p_list_head;
+    /*pushする前のリストを表示*/
+    show_all_data(p_list_head);
 
+    /*push処理*/
+    list *p_list_pointer = p_list_head;
 
     while(p_list_pointer->p_next != NULL){/*p->nextがリスト最後尾の状態へ*/
          p_list_pointer = p_list_pointer->p_next;  
     }
-    
-    
     
     p_list_pointer->p_next = p_new_data ;/*最後尾の状態となったp->nextにp_new_dataを格納*/
     
     p_new_data->p_next = NULL;
     p_new_data->value = 4;
 
-    printf("add  %d in the end of the list. \n",p_new_data->value);
+    /*pushした値を表示*/
+    printf("%d",p_new_data->value);
+    printf("  (address : %d) <-  NOW PUSHED \n",p_new_data); 
 
 }
+
+
+/* 220240310 要素を検索関数作成中 */
+void reference_data(list *p_list_head, int reference_value){
+    list *p_list_pointer = p_list_head;
+
+     int i_element = 0; /*リストの要素の順序*/
+     while(p_list_pointer != NULL){
+        i_element ++;
+        if(p_list_pointer->value == reference_value){
+            printf("The value of %d is in  %d place. \n",reference_value, i_element);
+        }
+        p_list_pointer = p_list_pointer->p_next; 
+    }
+}
+
 
 
 
@@ -63,7 +81,7 @@ int main() {
     show_all_data(p_list_head);
 
     push_data(p_list_head, &data4);
-
+    reference_data(p_list_head, 3);
     /*printf("push完了")；*/
     show_all_data(p_list_head);
    
