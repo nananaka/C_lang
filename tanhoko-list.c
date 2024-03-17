d2a0339c4b0cbfa6c570b4dce00ce7bfaaadd694
github user name <nakataso1029@gmail.com>
2024-03-17 13:17:53 +0900

    printfの中身を日本語化
    コンパイルの際に末尾に -fexec-charset=CP932をつければ文字化けしないことが分かったため。

--- tanhoko-list.c
+++ tanhoko-list.c
@@ -1,6 +1,5 @@
-// -*- coding: utf-8 -*-
-
 #include <stdio.h>
+
 
 typedef struct list_ {
     int value; /*値*/
@@ -11,10 +10,11 @@ typedef struct list_ {
 void show_all_data(list *p_list_head){
 
     list *p_list_pointer = p_list_head;
-    printf("-----------current_list--------------\n");
+    printf("-----------現在のリスト--------------\n");
+    printf("値 | アドレス\n");
     while(p_list_pointer != NULL){
         printf("%d",p_list_pointer->value);
-        printf("  (address : %d) \n",p_list_pointer); 
+        printf("  | %d \n",p_list_pointer); 
         p_list_pointer = p_list_pointer->p_next;
     }
 }
@@ -22,10 +22,6 @@ void show_all_data(list *p_list_head){
 
 /*リストの最後尾にデータを加える*/
 void push_data(list *p_list_head, list *p_new_data){
-
-
-    /*pushする前のリストを表示*/
-    show_all_data(p_list_head);
 
     /*push処理*/
     list *p_list_pointer = p_list_head;
@@ -39,9 +35,7 @@ void push_data(list *p_list_head, list *p_new_data){
     p_new_data->p_next = NULL;
     p_new_data->value = 4;
 
-    /*pushした値を表示*/
-    printf("%d",p_new_data->value);
-    printf("  (address : %d) <-  NOW PUSHED \n",p_new_data); 
+    printf("\n%dの値をリストの最後尾にデータを追加しました。\n\n",p_new_data->value); 
 
 }
 
@@ -54,7 +48,7 @@ void reference_data(list *p_list_head, int reference_value){
      while(p_list_pointer != NULL){
         i_element ++;
         if(p_list_pointer->value == reference_value){
-            printf("The value of %d is in  %d place. \n",reference_value, i_element);
+            printf("\n %dの値は %d 番目の要素にあります。 \n\n",reference_value, i_element);
         }
         p_list_pointer = p_list_pointer->p_next; 
     }
@@ -75,18 +69,20 @@ void delete_data(list *p_list_head, int list_order_number){
 
     /*削除したいデータの一つ手前のデータのp_nextを削除したいデータの一つ後につなげる。*/
     p_list_pointer->p_next = (p_list_pointer->p_next)->p_next;
-    printf("The %d data has been deleted.\n",list_order_number);
+    printf("\n%d番目のデータを削除しました。\n\n",list_order_number);
 
 }
 
 
 int main() {
+
     list *p_list_head; /*リストの先頭ポインタ*/
     list data1;
     list data2;
     list data3;
     list data4;
     
+    /*リストを作成*/
     p_list_head   = &data1;
     data1.value   = 1;
     data1.p_next  = &data2;
@@ -95,10 +91,10 @@ int main() {
     data3.value   = 3;
     data3.p_next  = NULL; 
 
-
-
+    /*リストを表示*/
     show_all_data(p_list_head);
 
+    /**/
     push_data(p_list_head, &data4);
     reference_data(p_list_head, 3);
     /*printf("push完了")；*/
